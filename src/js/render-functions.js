0fe0071a0671670908bf0e8c7fb545f773cc395d
Oksana Cherviachenko <kvintinovna@gmail.com>
2024-09-21 14:02:35 +0200

    update render-functions.js and styles.css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,10 +2,21 @@ export function renderGallery(images, gallery) {
   gallery.innerHTML = ''; 
 
   images.forEach(image => {
-    const imgElement = document.createElement('img');
-    imgElement.src = image.webformatURL;
-    imgElement.alt = image.tags;
-    imgElement.loading = 'lazy'; 
-    gallery.appendChild(imgElement);
+    const card = document.createElement('div');
+    card.classList.add('image-card');
+
+    card.innerHTML = `
+      <a href="${image.largeImageURL}" target="_blank">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p><b>Likes:</b> ${image.likes}</p>
+        <p><b>Views:</b> ${image.views}</p>
+        <p><b>Comments:</b> ${image.comments}</p>
+        <p><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    `;
+
+    gallery.appendChild(card);
   });
 }
