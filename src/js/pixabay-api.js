deacfa8cc48605bf0e11262f45caebeec5dab115
Oksana Cherviachenko <kvintinovna@gmail.com>
2024-09-26 15:47:55 +0200

    update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,19 @@
 const API_KEY = '46105418-b730233871b128ee828c4e337';
 const BASE_URL = 'https://pixabay.com/api/';
 
+import axios from 'axios';
+
 export async function fetchImages(query) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Failed to fetch images');
-    }
-    const data = await response.json();
-
+    const response = await axios.get(url);
     
-    if (data.hits.length === 0) {
+    if (response.data.hits.length === 0) {
       throw new Error('Sorry, there are no images matching your search query. Please try again!');
     }
 
-    return data.hits; 
+    return response.data.hits; 
   } catch (error) {
     console.error(error.message);
     alert(error.message);
